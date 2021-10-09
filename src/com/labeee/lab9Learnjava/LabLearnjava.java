72b49ab2a8fc8db4bd3f119e027cca74f3ae1fb7
Suparoek <39317424+Suparoek@users.noreply.github.com>
2021-10-09 14:47:36 +0700

    lab9

---
+++ src/com/labeee/lab9Learnjava/LabLearnjava.java
@@ -0,0 +1,14 @@
+package com.labeee.lab9Learnjava;
+
+public class LabLearnjava {
+	public static void main(String[] arges) {
+		for(int count=0;count<=20;count++) {
+			if(count ==11) {
+				System.out.println("รอบที่ i= "+count);
+				continue;
+			}
+				System.out.println("Hello");
+			}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module com.Labeee.lab8Learnjava {
+}
